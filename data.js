6eb95757cbbb1c46219be1f45e75ba48d6c78693
JavaScythe <slashmod6@gmail.com>
2022-10-16 11:33:19 +0000

    recursive gamesearch

--- data.js
+++ data.js
@@ -5,6 +5,22 @@ let config;
 function init(c){
 	config=c;
 }
+var walk = function(dir) {
+    var results = [];
+    var list = fs.readdirSync(dir);
+    list.forEach(function(file) {
+        file = dir + '/' + file;
+        var stat = fs.statSync(file);
+        if (stat && stat.isDirectory()) { 
+            /* Recurse into a subdirectory */
+            results = results.concat(walk(file));
+        } else { 
+            /* Is a file */
+            results.push(file);
+        }
+    });
+    return results;
+}
 function readyData(){
 	let bd = [];
 	let sdo = config["data"]["cache"];
@@ -22,12 +38,13 @@ function readyData(){
 	if(config["data"]["source"] == "folder" && nd){
 		let fn;
 		try{
-			fn = fs.readdirSync(__dirname+config["data"]["target"]);
+			fn = walk(__dirname+config["data"]["target"]);
 		} catch(e){
 			throw e;
 		}
 		for(var i in fn){
 			let n = fn[i];
+			n=n.split("/")[n.split("/").length-1];
 			n=n.replace(/_/g, " ");
 			n=n.replace(/-/g, " ");
 			n=n.split(".")
@@ -35,7 +52,7 @@ function readyData(){
 			n=n.join(".");
 			bd.push({
 				"title": n,
-				"location": config["data"]["location"]+fn[i]
+				"location": fn[i].replace(__dirname, "")
 			});
 		}
 	}
--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const config = require("./config.js");
 const api = require("./api.js");
 console.log(`Loaded ${api.init(config)} games in ${new Date().getTime()-startTime}ms`);
 app.use("/api/:method", (req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	let d = api.consume(req.params.method, req.query);
 	res.type('json');
 	res.send(d);
 });
 app.get("/", (req, res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.send("l bozo");
 });
 app.listen(3000);
